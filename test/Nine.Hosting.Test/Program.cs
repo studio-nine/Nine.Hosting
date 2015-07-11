c519057ae321576cdca7ef4393e2f98f0c8be8e5
Yufei Huang <yufeih@live.com>
2015-07-11 16:53:07 +0800

    Add a sample windows forms project.

---
+++ test/Nine.Hosting.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Nine.Hosting
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public class Program
+    {
+        private readonly IHostWindow hostWindow;
+
+        public Program(IHostWindow hostWindow)
+        {
+            this.hostWindow = hostWindow;
+        }
+
+        public void Main(string[] args)
+        {
+            // Run "dnx --watch . dev" in this directory
+            // Run "dnx --watch --port [PORT] . dev" to enable faster 
+            // reload using Visual Studio compiler service.
+            //
+            // You can then modify the following code the see the changes
+            // taking effect as you type and hit Ctrl+S.
+            var form = new Form { BackColor = Color.DarkGray };
+            hostWindow?.Attach(form.Handle);
+            Application.Run(form);
+        }
+    }
+}
